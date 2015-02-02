57a961f667b696a3f123d005732c68a4b8b1c827
rezahadafi <rezahadafi@gmail.com>
2015-02-02 15:17:33 +0700

    updated

--- Promos@ID/Promos@ID/AppDelegate.h
+++ Promos@ID/Promos@ID/AppDelegate.h
@@ -7,30 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
-<<<<<<< HEAD
-#import <CoreData/CoreData.h>
-=======
 @import CoreData;
->>>>>>> 58642eb9846fbb2e08dc114081c4c65d5e2f2c9d
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-//@property (strong, nonatomic) CoreDataSaveArrayViewController *viewController;
 {
     NSMutableString *location;
 	NSManagedObjectModel *managedObjectModel;
 	NSManagedObjectContext *managedObjectContext;
 	NSPersistentStoreCoordinator *persistentStoreCoordinator;
 }
+
 @property (strong, nonatomic) UIWindow *window;
-<<<<<<< HEAD
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
-@property (nonatomic, retain) NSString *location;
-=======
 @property (nonatomic, retain) NSMutableString *location;
 
 @property (nonatomic, strong, readonly) NSManagedObjectModel *managedObjectModel;
@@ -38,7 +25,6 @@
 @property (nonatomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 - (NSURL *)applicationDocumentsDirectory;
->>>>>>> 58642eb9846fbb2e08dc114081c4c65d5e2f2c9d
 
 @end
 
